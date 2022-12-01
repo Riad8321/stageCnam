f877a7082e5178dbf3d3703b58c916408719c07a
r.draoui <riad.draoui@gmail.com>
2022-12-01 09:12:09 +0100

    some modifications

--- stage-cnam/src/components/Hero/Hero.jsx
+++ stage-cnam/src/components/Hero/Hero.jsx
@@ -5,8 +5,7 @@ import region from '../../assets/Logo-Region-min.jpg'
 import tvt from '../../assets/Logo-Tvt-min.png'
 import tpm from '../../assets/Logo-Tpm-min.png'
 import lft from '../../assets/Logo-Lft-min.png'
-import boiteImmo from '../../assets/maxresdefault.jpg'
-import bandeau2038 from '../../assets/Bandeau2038.png'
+import heroImage from '../../assets/hero-image.jpg'
 
 
 import '../Hero/hero.css'
@@ -25,10 +24,13 @@ export default function Hero() {
                 <img className='lft' src={lft} alt="lft" />
                 <img className='tpm' src={tpm} alt="tpm" />
             </div>
-            <div className="cnam2038">
-                <h2>Stage</h2>
-                <p>Bonjour, moi c'est Riad </p>
+            <div className="hero-text">
+                <h2></h2>
+                <p>Bonjour, moi c'est Riad, j'ai effectué mon stage dans la société La Boite Immo, située à Hyères </p>
                 <p></p>
+            </div>
+            <div className="hero-image">
+            <img className='heroImage' src={heroImage} alt="heroImage" />
             </div>
         </div>
     </>
