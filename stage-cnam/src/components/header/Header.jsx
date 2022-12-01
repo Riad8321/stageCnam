ae8fce5594f424200227745eeda6805da12d242d
r.draoui <riad.draoui@gmail.com>
2022-12-01 15:10:32 +0100

    add React Router

--- stage-cnam/src/components/header/Header.jsx
+++ stage-cnam/src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import logoWeb from '../../assets/logoWeb.png'
 import '../header/header.css'
 export default function Header() {
@@ -6,9 +7,15 @@ export default function Header() {
     <nav className='nav-bar'>
        <img className='logoWeb' src={logoWeb} alt="logo" />
        <ul className='menu-center'>
-           <li><a href={"#"}>HOME</a></li>
-           <li><a href={"#"}>ENTREPRISE</a></li>
-           <li><a href={"#"}>STAGE</a></li> 
+           <li>
+            <a href={"#"}>HOME</a>
+            </li>
+           <li>
+            <a href={"#"}>ENTREPRISE</a>
+            </li>
+           <li>
+            <a href={"#"}>STAGE</a>
+            </li> 
        </ul>
     </nav>
   )
