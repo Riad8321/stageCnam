45d6920e28902f400091fc7b29828509e7a81caa
Riad8321 <112904701+Riad8321@users.noreply.github.com>
2022-12-05 22:06:37 +0100

    //

--- stage-cnam/src/components/Entreprise/Entreprise.jsx
+++ stage-cnam/src/components/Entreprise/Entreprise.jsx
@@ -1,16 +1,12 @@
-import Slider from 'react-slick';
-import Slide from 'react-slick';
 import React from 'react'
 import Header from '../home/header/Header'
 import Hero from '../Entreprise/entrepriseComponents/entrepriseHero'
-import Slideshow from './entrepriseComponents/slider/Slider'
 import './entreprise.css'
 
 export default function entreprise() {
   return (
     <div className='entreprise-css'> 
         <Header />
-        <Slideshow />
     </div>
   )
 }
