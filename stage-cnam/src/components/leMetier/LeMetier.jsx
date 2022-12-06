08a3cdcfcbc88de3d53bad48a1560f4d55e97204
Riad8321 <112904701+Riad8321@users.noreply.github.com>
2022-12-06 22:42:54 +0100

    add cards

--- stage-cnam/src/components/leMetier/LeMetier.jsx
+++ stage-cnam/src/components/leMetier/LeMetier.jsx
@@ -7,6 +7,14 @@ export default function LeMetier() {
     <div className="le-metier">
       <Header />
         <div>Le metier</div>
+        <p>Le développeur web est un professionnel chargé de créer et de maintenir des sites et des applications web. Il utilise des langages de programmation pour développer des interfaces utilisateur, des bases de données et des algorithmes de traitement des données. Le développeur web travaille en étroite collaboration avec les designers et les autres professionnels de l'informatique pour créer des sites web qui répondent aux besoins des utilisateurs et des clients. Il doit être capable de travailler de manière autonome et de gérer plusieurs projets en même temps.</p>
+        <div className="heroCorps">
+          <div className="div1">1</div>
+          <div className="div2">2</div>
+          <div className="div3">3</div>
+          <div className="div3">3</div>
+          <div className="div3">3</div>
+        </div>   
     </div>
   )
 }
